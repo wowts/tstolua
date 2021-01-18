bab1d1ebdc2d281edee6b84af9630e40773ae65b
Sidoine De Wispelaere <sidoine@sidoine.net>
2021-01-18 09:40:05 +0100

    chore: upgrade packages

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { option } from "commander";
 import { compile } from "./compiler";
 
-const options = option("-j, --js", "Emit javascript")
+const program = option("-j, --js", "Emit javascript")
     .option("-w, --watch", "Watch mode")
     .option(
         "-p, --project [tsconfig.json]",
@@ -9,7 +9,7 @@ const options = option("-j, --js", "Emit javascript")
         "./tsconfig.json"
     )
     .option("-i, --install", "Install lua dependencies")
-
     .parse(process.argv);
 
+const options = program.opts();
 compile(options.project, options.watch, options.js, options.install);
